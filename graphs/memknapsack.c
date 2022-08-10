019b65bf6176665b0e58f57dabbab071151555f8
Shivam Menda <shivammenda2002@gmail.com>
2022-08-10 23:23:49 +0530

    DFS topo sort added.

--- graphs/memknapsack.c
+++ graphs/memknapsack.c
@@ -1,52 +1,88 @@
-#include <stdio.h>
-int table[100][100];
-int v[100],w[100];
-
-int max(int a, int b)
-{
-    return (a > b) ? a : b;
+#include<stdio.h>
+ 
+#include<stdlib.h>
+ 
+ 
+int n,table[100][100],cap,wt[100]={0},val[100]={0};
+ 
+ 
+int max(int a,int b){
+ 
+    return (a>b)?a:b;
+ 
 }
-
-int mknap(int i,int j)
-{
+ 
+ 
+int knapsack(int i,int j){
+ 
     int value;
-    if(table[i][j]<0)
-    {
-        if(j<w[i])
-        {
-            
-            value=mknap(i-1,j);
+ 
+ 
+    if(table[i][j]== -1){
+ 
+        if(j<wt[i])
+ 
+            value = knapsack(i-1,j);
+ 
+ 
+        else{
+ 
+            value = max(knapsack(i-1,j),val[i]+knapsack(i-1,j-wt[i]));
+ 
         }
-        else
-        {
-            value=max(mknap(i-1,j),v[i]+mknap(i-1,(j-w[i])));
-        }
+ 
         table[i][j]=value;
-        
+ 
     }
+ 
     return table[i][j];
+ 
 }
-
-int main()
-{
-    int n, capacity;
+ 
+ 
+void main(){
+ 
     printf("Enter the number of items: ");
-    scanf("%d", &n);
-    printf("Enter the capacity: ");
-    scanf("%d", &capacity);
-    for (int i = 0; i < n; i++)
-    {
-        printf("\nEnter value and weight for %d item: ", i + 1);
-        scanf("%d %d", &v[i], &w[i]);
+ 
+    scanf("%d",&n);
+ 
+ 
+    printf("Enter capacity of sack: ");
+ 
+    scanf("%d",&cap);
+ 
+ 
+    for(int i=1;i<=n;i++){
+ 
+        printf("Enter the weight of item %d: ",i);
+ 
+        scanf("%d",&wt[i]);
+ 
+ 
+        printf("Enter the value of item %d: ",i);
+ 
+        scanf("%d",&val[i]);
+ 
     }
-    for (int j = 0; j <=capacity; j++)
-        table[0][j] = 0;
-    for (int i = 0; i <=n; i++)
-        table[i][0] = 0;
-    for (int i = 1; i <=n; i++)
-        for (int j = 1; j <=capacity; j++)
-            table[i][j] = -1;
-    
-    printf("Max value: %d", mknap(n-1, capacity));
-    return 0;
+ 
+ 
+    for(int i=0;i<=n;i++){
+ 
+        for(int j=0;j<=cap;j++){
+ 
+            if(i==0 || j==0)
+ 
+                table[i][j]=0;
+ 
+            else
+ 
+                table[i][j]=-1;
+ 
+        }
+ 
+    }
+ 
+ 
+    printf("Maximum value of knapsack is : %d",knapsack(n,cap));
+ 
 }
